ae2f5ccfb7d72b7b895c04e0236c6f9cf1ddc71b
BinteZiker <128366946+BinteZiker@users.noreply.github.com>
2023-05-08 16:05:36 +0500

    Add files via upload

---
+++ ass 3/app.js
@@ -0,0 +1,34 @@
+// //                ASSIGNMNET 3 
+// //       ======== QUESTION 1     =========
+
+// let age = 18;
+// alert("i am " + age + " years old");
+
+// //          ======== QUESTION 2     =========
+
+// let number_of_visits = 0;
+// let i = 0;
+// for(i=0;i<1000;i++){
+//     number_of_visits = number_of_visits+1;
+//     alert("you have visited this site " + number_of_visits + " times")
+// }
+
+// function visits(number_of_visits){
+//     return number_of_visits;
+// }
+
+
+// //           ======== QUESTION 3     =========
+
+// let birth_year = 2004;
+// document.write("m birth year is " + birth_year );
+// document.write("<br>")
+// document.write("data type of my declared variable is number")
+
+
+// //              ======== QUESTION 4     =========
+// let client_name = "alisha fatima";
+// let no_of_orders = 11;
+// let product_title = "shoes";
+
+// document.write( "<b>" + client_name  + "</b>"+ " ordered " + "<b>"+ no_of_orders  + " "+ product_title +  "</b>(s)"+ " on xyz clothing store");
---
+++ ass 4/app.js
@@ -0,0 +1,41 @@
+//                               ASSIGNMENT 4 
+//        ======================  QUESTION 1     ==========================
+
+// let name = "alisha";  let age = 17; let height = "5.2f";
+
+
+// //        =======================  QUESTION 2    =========================
+
+// // legal variables
+
+// let my_name = "alisha";
+// let age_1 = 18;
+// let grade = "eleven";
+// let phoneNumber = "012345678";
+// let $marks = "100";
+
+// // illegal variables
+
+// let 1name = "alisha";  //number in start illegal
+// let my age = 11;  // space in between illegal
+// let function = "hello world";  //reserved keyword
+// let address# = "abcdef397"; // use of # is not allowed
+// let marks+percentage = 43;  // use of + and all special char except $ , _ are not allowed
+
+// //        =========  QUESTION 2     ===============
+// let number = " number "
+// let _ = " _ ";
+// let $ = " & ";
+// let alphabet = " letters "
+// let sensitivity = "sensitive";
+// let keyword = "reserved keyword"
+
+// document.write( "<b> Rules for naming JS variables </b> <br><br>")
+
+// document.write("Variable names can only contain "+ number + ", "+ _  + ", "+ $  + "and " +  alphabet + ", For example :$my_1stVariable "+"<br>")
+
+// document.write("Variables must begin with a "+  number  + _  + ", "+ $  + "and " +  alphabet + ", For example :$name, _name or name <br>")
+
+// document.write("Variable names are case " + sensitivity + "<br> ");
+
+// document.write("Variable names should not be JS  "+ keyword)
